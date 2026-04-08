3df4a6602c8d3446b69364ea973976ba4082b6e0
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-04-08 23:05:53 +0200

    design sign in and sign up pages

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -1,9 +1,32 @@
+import Btn from "../component/btn";
+import { MdArrowBack } from "react-icons/md";
+
 function SignIn() {
+  function signin() {}
   return (
-    <div>
-      
+    <div className="relative min-h-screen auth-page flex justify-center items-center py-10 sm:px-10 px-5">
+      <div className="flex items-center gap-1.5 border-b border-b-primary text-primary absolute top-5 left-5">
+        <MdArrowBack />
+        <a href="/home">Go Home</a>
+      </div>
+      <div className="flex flex-col items-center h-fit md:w-150 w-full py-20 sm:px-10 px-7 rounded-2xl bg-[#0000001f]">
+        <img src="/images/logo.png" alt="logo" className="w-50" />
+        <div className="w-full mt-10">
+          <form onSubmit={signin}>
+            <input type="email" placeholder="Email" className="mb-5" />
+            <input type="password" placeholder="Password" className="mb-10" />
+            <Btn className="rounded-3xl w-full text-light">Sign In</Btn>
+          </form>
+        </div>
+        <div className="mt-7">
+          <span className="text-light">New to BidSphere?&nbsp;</span>
+          <a href="/auth/signup" className="text-primary underline text-center">
+            Create New Account
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,9 +1,54 @@
+import { useState } from "react";
+import Btn from "../component/btn";
+import { MdArrowBack } from "react-icons/md";
+
 function SignUp() {
+  const [type, setType] = useState("USER");
+
+  function signup() {}
   return (
-    <div>
-      
+    <div className="min-h-screen auth-page flex justify-center items-center py-15 sm:px-10 px-5">
+      <div className="flex items-center gap-1.5 border-b border-b-primary text-primary absolute top-5 left-5">
+        <MdArrowBack />
+        <a href="/home">Go Home</a>
+      </div>
+      <div className="flex flex-col items-center h-fit md:w-150 w-full pt-20 pb-10 sm:px-10 px-7 rounded-2xl bg-[#0000001f]">
+        <img src="/images/logo.png" alt="logo" className="w-50" />
+        <div className="w-full mt-10">
+          <form onSubmit={signup}>
+            <input type="text" placeholder="Name" className="mb-5" />
+            <input type="email" placeholder="Email" className="mb-5" />
+            <input type="text" placeholder="Phone Number" className="mb-5" />
+
+            <input type="password" placeholder="Password" className="mb-5" />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              className="mb-5"
+            />
+            <div className="flex items-center gap-5 mb-10">
+              <p className="text-surface-500">Type</p>
+              <div className="flex bg-surfacetonal-200 p-1 rounded-3xl w-fit">
+                {["USER", "SELLER"].map((item) => (
+                  <button
+                    className={`px-5 py-1.5 rounded-2xl text-surface-500 ${type === item ? "bg-primary" : ""}`}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setType(item);
+                    }}
+                    key={item}
+                  >
+                    {item}
+                  </button>
+                ))}
+              </div>
+            </div>
+            <Btn className="rounded-3xl w-full text-light">Sign Up</Btn>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
