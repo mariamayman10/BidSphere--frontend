479fae5717019d5aded8522afed6a20a0cc70827
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-04-20 14:06:59 +0200

    update the dropdown link hover design

--- src/component/navbar/dropdownCol.jsx
+++ src/component/navbar/dropdownCol.jsx
@@ -4,7 +4,7 @@ function DropdownCol({ arr, param, paramValue }) {
   return (
     <>
       <p className="text-surface-400">{param}</p>
-      <div className="w-full h-[0.2px] bg-surface-500 my-2"></div>
+      <div className="w-full h-[0.2px] bg-surface-400 my-2"></div>
       <div className="flex flex-col gap-1.5">
         {arr.map((item) => {
           const value = item.toLowerCase();
@@ -13,9 +13,14 @@ function DropdownCol({ arr, param, paramValue }) {
             <Link
               key={item}
               to={`auctions?${param.toLowerCase()}=${item.toLowerCase()}`}
-              className={`block ${isActive ? "active" : ""}`}
+              className={`group/item block ${isActive ? "active" : ""} hover:bg-surface-200 px-2 py-1 rounded-sm`}
             >
-              {item}
+              <div className="relative flex items-center">
+                <div className="absolute left-0 right-5 h-[1.5px] w-4 bg-primary origin-left scale-x-0 group-hover/item:scale-x-100 transition-transform duration-200 ease-out"></div>
+                <span className="transition-transform duration-200 group-hover/item:translate-x-5">
+                  {item}
+                </span>
+              </div>
             </Link>
           );
         })}
